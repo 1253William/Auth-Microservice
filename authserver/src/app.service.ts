2a6fb0332f145aa3c260ab60d105313f5cd7048c
1253William <williamofosuparwar@gmail.com>
2026-01-14 17:57:32 +0000

    fix: prisma downgrade

--- authserver/src/app.service.ts
+++ authserver/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Server Running on Port 3000';
+    return 'Hello World!';
   }
 }
